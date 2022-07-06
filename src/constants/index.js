3092961d9333893cae990bba67854c6be5202919
Dwain Robinson <robinson.dwain.developer@gmail.com>
2022-07-05 23:05:20 -0500

    Updating constants file

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,15 @@
 'use strict';
 
-export const DEFAULT_FILE_EXTENTION = 'pdf';
+export const PDF_FILE_EXTENTION = 'pdf';
 
-export const DEFAULT_SUBSCRIPTION_TYPE = 'issue';
+export const PDF_SUBSCRIPTION_TYPE = 'issue';
 
-export const DEFAULT_MIME_TYPE = 'application/pdf';
+export const PDF_MIME_TYPE = 'application/pdf';
+
+export const VIDEO_FILE_EXTENTION = 'mp4';
+
+export const VIDEO_SUBSCRIPTION_TYPE = 'video';
+
+export const VIDEO_MIME_TYPE = 'video/mp4';
 
 export const DEFAULT_TIME_FORMAT = 'YYYY-MM-DD[T]HH:mm:ss';
