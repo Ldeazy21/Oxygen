9b685649863ee0aec9d0be108acdb02f7e4a5736
Dwain Robinson <robinson.dwain.developer@gmail.com>
2022-07-08 20:05:11 -0500

    Adding error case for subscription not found

--- src/services/SubscriptionService.js
+++ src/services/SubscriptionService.js
@@ -37,6 +37,7 @@ exports.getSubscription = async subscriptionId => {
         }
       ];
     }
+    return badRequest(`No subscriptions found with id: ${subscriptionId}.`);
   } catch (err) {
     console.log('Error getting remaining time on subscription: ', err);
     return badImplementationRequest(
