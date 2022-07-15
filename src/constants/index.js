a8b6116089501da27863d1f2ec6d0a00943819ff
Dwain Robinson <robinson.dwain.developer@gmail.com>
2022-07-15 10:49:23 -0500

    Adding validation for subscription type and package.json

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,3 +5,7 @@ export const VIDEO_SUBSCRIPTION_TYPE = 'video';
 export const ISSUE_SUBSCRIPTION_TYPE = 'issue';
 
 export const DEFAULT_TIME_FORMAT = 'YYYY-MM-DD[T]HH:mm:ss';
+
+export const SUBSCRIPTION_TYPES = ['issue', 'video'];
+
+export const RECURRING_TYPES = ['monthly', 'yearly'];
--- src/models/Subscription.js
+++ src/models/Subscription.js
@@ -7,16 +7,18 @@ import { createSubscriptionId } from '../utilities';
 const { Schema } = mongoose;
 const { NODE_ENV } = config;
 
-//ISSUE SCHEMA
+import { SUBSCRIPTION_TYPES, RECURRING_TYPES } from '../constants';
+
+//SUBSCRIPTION SCHEMA
 //  ============================================
 const subscriptionSchema = new Schema({
   subscriptionId: { type: String, default: createSubscriptionId() },
   userId: { type: Number, required: true },
   username: { type: String, required: true },
   email: { type: String, required: true },
-  type: { type: String, required: true },
+  type: { type: String, required: true, enum: SUBSCRIPTION_TYPES },
   purchaseDate: { type: String },
-  recurring: { type: String },
+  recurring: { type: String, enum: RECURRING_TYPES },
   amount: { type: Number, default: 15 },
   startDate: { type: String },
   endDate: { type: String }
