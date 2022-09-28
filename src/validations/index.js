2a916142c98bf147bfa64924a384e554f549c167
Abdul <nomadscru@gmail.com>
2022-09-28 16:12:42 +0500

    rremvove

--- src/validations/index.js
+++ src/validations/index.js
@@ -59,11 +59,6 @@ const subscriptionStatusQueryValidation = [
     .isString()
     .withMessage('Must provide a existing subscription id.')
 ];
-const subscriptionQueryValidation = [
-  query('subscriptionId')
-    .isString()
-    .withMessage('Must provide a existing subscription id.')
-];
 
 const subscriptionUpdateBodyValidation = [
   body('subscriptionId')
